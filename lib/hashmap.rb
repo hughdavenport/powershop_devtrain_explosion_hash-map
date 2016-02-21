78bc9993dcfd52f9b92e4778f1974ed159463cd6
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-22 09:56:05 +1300

    Add test for default constructor

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,3 +1,12 @@
 class HashMap
 
+    def initialize
+        @data = Array.new(10)
+        @size = 0
+    end
+
+    def empty?
+        @size == 0
+    end
+
 end
--- spec/hashmap_spec.rb
+++ spec/hashmap_spec.rb
@@ -2,4 +2,15 @@ require 'HashMap'
 
 RSpec.describe HashMap do
 
+    describe "When I create a new hashmap" do
+        context "with no arguments" do
+            before do
+                @map = HashMap.new
+            end
+            it "should be empty" do
+                expect(@map).to be_empty
+            end
+        end
+    end
+
 end
