d055a6b6135cc0bf971db1c6f233cea41b5073a1
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-22 10:05:37 +1300

    Build size by number of keys

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,16 +1,19 @@
 class HashMap
 
     attr_reader :weight
-    attr_reader :size
 
     def initialize
+        @keys = []
         @data = Array.new(10)
-        @size = 0
         @weight = 0.8
     end
 
     def empty?
-        @size == 0
+        size == 0
+    end
+
+    def size
+        @keys.length
     end
 
     def underlying_size
